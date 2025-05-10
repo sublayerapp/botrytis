ddd0c047b3e8b1b2ae33667b94e4e95ab15954d4
Scott Werner <stwerner@vt.edu>
2025-05-10 14:58:28 -0400

    initial commit

---
+++ lib/botrytis.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "botrytis/version"
+
+module Botrytis
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/botrytis_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Botrytis do
+  it "has a version number" do
+    expect(Botrytis::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
